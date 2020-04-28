5e14e503f050d91de1d1f5bb55132d4cb26783dc
Virgilio Calderón <Virgilio@MacBook-Air-Virgilio.local>
2020-04-27 20:25:09 -0400

    ruta actualizada y controller de usuario

---
+++ rest-api/app/Controllers/Http/UserController.js
@@ -0,0 +1,11 @@
+'use strict'
+
+class UserController {
+   store(){
+       return{
+           mensaje: 'Creamos un usuario en el controlador'
+       }
+   } 
+};
+
+module.exports = UserController
--- rest-api/start/routes.js
+++ rest-api/start/routes.js
@@ -16,6 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.group(()=>{
+  Route.post('/user/register', 'UserController.store');
+}).prefix('api/v2');
+
