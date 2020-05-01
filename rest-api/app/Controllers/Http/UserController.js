333e2d94d9783f737b6f3ecc59fbb8155eba4b0c
Virgilio Calderón <Virgilio@MacBook-Air-Virgilio.local>
2020-05-01 18:27:47 -0400

    Registro de usuarios en sqlite3

--- rest-api/app/Controllers/Http/UserController.js
+++ rest-api/app/Controllers/Http/UserController.js
@@ -1,11 +1,18 @@
 'use strict'
 
+const User = use('App/Models/User');
+
 class UserController {
-   store(){
-       return{
-           mensaje: 'Creamos un usuario en el controlador'
-       }
-   } 
-};
+   store({request}){
+       const {email, password } = request.all();
+       console.log(email, password);
+       const user = User.create ({
+           username: email,
+           email,
+           password
+       });
+       return user;
+   };
+}
 
 module.exports = UserController
